115e912499e42f64e496943910cfcc4b291a1ae5
Anton Berdnikov <ant17893@mail.ru>
2020-08-03 23:35:12 +0600

    first commit

---
+++ src/main/java/ru/netology/domain/Coordinates.java
@@ -0,0 +1,6 @@
+package ru.netology.domain;
+
+public class Coordinates {
+    private String type;
+    private String coordinates;
+}
---
+++ src/main/java/ru/netology/domain/User.java
@@ -0,0 +1,8 @@
+package ru.netology.domain;
+
+public class User {
+    private boolean Liked;
+    private boolean Reposted;
+    private boolean commentsAllowed;
+    private boolean bookmarked;
+}
